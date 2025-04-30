af6c59082aef47fb4c7fbe81b969287dc1088092
ismail <ismailbouras1968@gmail.com>
2025-04-30 11:25:09 +0200

    cors

--- src/main/java/ma/entraide/formation/config/SecurityConfig.java
+++ src/main/java/ma/entraide/formation/config/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://http://154.144.246.177:8040")); // Add your frontend URL
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://154.144.246.177:8040")); // Add your frontend URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
         configuration.setAllowCredentials(true);
